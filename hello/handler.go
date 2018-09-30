4809aced1f9f50896f1c834695ce87ceed74ed9e
Philippe Charrière <pcharriere@gitlab.com>
2018-09-30 08:42:57 +0200

    :sparkles: hello function
    Signed-off-by: Philippe Charrière <pcharriere@gitlab.com>

--- hello/handler.go
+++ hello/handler.go
@@ -1,25 +1,12 @@
 package function
 
 import (
-	"encoding/json"
 	"fmt"
 )
-
-type Message struct {
-	who string `json:"who"`
-	what string `json:"what"`
-	text string `json:"text"`
-}
 
 // Handle a serverless request...
 func Handle(req []byte) string {
 
-	message := json.Marshal(Message{
-		who: "Jay",
-		what: "🤖",
-		text: "👋 I ❤️ OpenFaaS"
-	})
-
 	//return string(message)
-	return fmt.Sprintf("%s", string(message))
+	return fmt.Sprintf("👋 🌍 I ❤️ OpenFaaS")
 }
