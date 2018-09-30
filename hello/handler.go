33c410892333ce23bd9a04dbfa55a29e57a25b76
Philippe Charrière <pcharriere@gitlab.com>
2018-09-30 08:05:46 +0200

    :sparkles: my 1st Go function

---
+++ hello/handler.go
@@ -0,0 +1,25 @@
+package function
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Message struct {
+	who string `json:"who"`
+	what string `json:"what"`
+	text string `json:"text"`
+}
+
+// Handle a serverless request
+func Handle(req []byte) string {
+
+	message := json.Marshal(Message{
+		who: "Jay",
+		what: "🤖",
+		text: "👋 I ❤️ OpenFaaS"
+	})
+
+	return string(message)
+	//return fmt.Sprintf("%s", string(message))
+}
